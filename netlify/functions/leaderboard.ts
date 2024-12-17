0ec5c4643431ed477a78d5aae053bb55608bccf6
Matt Tingen <matt.tingen@gmail.com>
2024-12-17 17:42:44 -0500

    round ms

--- netlify/functions/leaderboard.ts
+++ netlify/functions/leaderboard.ts
@@ -1,6 +1,12 @@
 import type { Context } from '@netlify/functions';
 import dedent from 'dedent';
+import { round } from 'es-toolkit';
 import { query } from '../query';
+
+interface LeaderboardRow {
+  name: string;
+  ms: number;
+}
 
 // eslint-disable-next-line import/no-default-export
 export default async (req: Request, context: Context) => {
@@ -16,6 +22,11 @@ export default async (req: Request, context: Context) => {
   });
 
   return result.success
-    ? context.json(result.result[0].results)
+    ? context.json(
+        (result.result[0].results as LeaderboardRow[]).map((row) => ({
+          ...row,
+          ms: round(row.ms, -1),
+        })),
+      )
     : new Response(null, { status: 500 });
 };
