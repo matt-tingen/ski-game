912f79d671f6a2b6d1f196e230cad349142953d1
Matt Tingen <matt.tingen@gmail.com>
2024-12-17 20:28:01 -0500

    add error msg for load failure

--- src/elements.ts
+++ src/elements.ts
@@ -1,6 +1,13 @@
+export const canvasContainer = document.getElementById(
+  'container',
+) as HTMLButtonElement;
+export const volumeContainer = document.getElementById(
+  'volume-container',
+) as HTMLButtonElement;
 export const leftTurnButton = document.getElementById(
   'left',
 ) as HTMLButtonElement;
 export const rightTurnButton = document.getElementById(
   'right',
 ) as HTMLButtonElement;
+export const loadingError = document.getElementById('loading-error')!;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { once } from 'es-toolkit';
 import { Color, DisplayMode, Engine, ImageFiltering } from 'excalibur';
-import { leftTurnButton, rightTurnButton } from './elements';
+import {
+  canvasContainer,
+  leftTurnButton,
+  loadingError,
+  rightTurnButton,
+  volumeContainer,
+} from './elements';
 import { MyLevel } from './level';
 import { MainMenu } from './mainMenu';
 import { Reset } from './reset';
@@ -19,58 +25,69 @@ const requestMusic = () => {
   }
 };
 
-const game = new Engine({
-  width: 300,
-  height: 800,
-  canvasElementId: 'game',
-  backgroundColor: Color.Transparent,
-  displayMode: DisplayMode.FitContainer,
-  pixelArt: true,
-  antialiasing: {
-    filtering: ImageFiltering.Pixel,
-  },
-  scenes: {
-    game: MyLevel,
-    menu: MainMenu,
-    reset: Reset,
-  },
-  fixedUpdateFps: new URL(location.toString()).searchParams.get('fixed')
-    ? Number(new URL(location.toString()).searchParams.get('fixed'))
-    : undefined,
-  suppressPlayButton: true,
-  configurePerformanceCanvas2DFallback: {
-    allow: true,
-    showPlayerMessage: true,
-  },
-});
+const onError = () => {
+  loadingError.classList.remove('hidden');
+  canvasContainer.classList.add('hidden');
+  volumeContainer.classList.add('hidden');
+};
 
-game
-  .start('menu', {
-    loader,
-  })
-  .then(() => {
-    requestMusic();
+try {
+  const game = new Engine({
+    width: 300,
+    height: 800,
+    canvasElementId: 'game',
+    backgroundColor: Color.Transparent,
+    displayMode: DisplayMode.FitContainer,
+    pixelArt: true,
+    antialiasing: {
+      filtering: ImageFiltering.Pixel,
+    },
+    scenes: {
+      game: MyLevel,
+      menu: MainMenu,
+      reset: Reset,
+    },
+    fixedUpdateFps: new URL(location.toString()).searchParams.get('fixed')
+      ? Number(new URL(location.toString()).searchParams.get('fixed'))
+      : undefined,
+    suppressPlayButton: true,
+    configurePerformanceCanvas2DFallback: {
+      allow: true,
+      showPlayerMessage: true,
+    },
   });
 
-initVolume();
+  game
+    .start('menu', {
+      loader,
+    })
+    .then(() => {
+      requestMusic();
+    })
+    .catch(() => {});
 
-// https://developer.mozilla.org/en-US/docs/Web/Security/User_activation
-['keydown', 'mousedown', 'pointerdown', 'pointerup', 'touchend'].forEach(
-  (e) => {
-    document.addEventListener(e, requestMusic);
-  },
-);
+  initVolume();
 
-[leftTurnButton, rightTurnButton].forEach((btn) => {
-  const onButtonDown = () => {
-    btn.setAttribute('data-active', 'true');
-  };
+  // https://developer.mozilla.org/en-US/docs/Web/Security/User_activation
+  ['keydown', 'mousedown', 'pointerdown', 'pointerup', 'touchend'].forEach(
+    (e) => {
+      document.addEventListener(e, requestMusic);
+    },
+  );
 
-  const onButtonUp = () => {
-    btn.removeAttribute('data-active');
-  };
+  [leftTurnButton, rightTurnButton].forEach((btn) => {
+    const onButtonDown = () => {
+      btn.setAttribute('data-active', 'true');
+    };
 
-  btn.addEventListener('pointerdown', onButtonDown);
-  btn.addEventListener('pointerup', onButtonUp);
-  btn.addEventListener('pointercancel', onButtonUp);
-});
+    const onButtonUp = () => {
+      btn.removeAttribute('data-active');
+    };
+
+    btn.addEventListener('pointerdown', onButtonDown);
+    btn.addEventListener('pointerup', onButtonUp);
+    btn.addEventListener('pointercancel', onButtonUp);
+  });
+} catch {
+  onError();
+}
