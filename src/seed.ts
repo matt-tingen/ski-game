1d1618785a4412654c7588936248a28df9fc3874
Matt Tingen <matt.tingen@gmail.com>
2024-12-19 09:48:42 -0500

    Fix seed

--- src/seed.ts
+++ src/seed.ts
@@ -1,6 +1,6 @@
 import { searchParams } from './searchParams';
 
 const formatDate = (date: Date) =>
-  `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
 export const getSeed = () => searchParams.get('seed') || formatDate(new Date());
